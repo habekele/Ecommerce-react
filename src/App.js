8f62e581cf5e801f28eea31d9606cb36618fe6ec
henokbekele <henokbekele1997@gmail.com>
2023-04-26 23:01:48 -0500

    added shop and cart pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
 
 
 function App() {
@@ -9,8 +11,8 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path='/'/>
-          <Route path='/cart'/>
+          <Route path="/" element={<Shop/>}/>
+          <Route path="/cart" element={<Cart/>}/>
         </Routes>
       </Router>
     </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {ShoppingCart} from 'phosphor-react'
+import{House} from 'phosphor-react'
+import "./navbar.css"
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='links'>
+            <Link to="/">
+                <House size={32}/>
+            </Link>
+            <Link to="/cart">
+                <ShoppingCart size={32}/>
+            </Link>
+
+        </div>
+    </div>
   )
 }

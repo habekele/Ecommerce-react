cb7f308593bb1a6632395527272746bcbbe315aa
henokbekele <henokbekele1997@gmail.com>
2023-04-27 23:03:20 -0500

    product list partially done

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import {PRODUCTS} from '../../products'
+import { Product } from './product'
 
 export const Shop = () => {
   return (
-    <div>Shop</div>
+    <div className='shop'>
+        
+        <div className='shopTitle'>
+            <h1>Henok's Store</h1>
+        </div>
+        <div className='products'>
+            {PRODUCTS.map((product) => (
+            <Product data={product}/>
+            ))}
+        </div>
+    </div>
   )
 }
---
+++ src/products.js
@@ -0,0 +1,35 @@
+import product1 from './assets/iphone.png'
+import product2 from './assets/macbook.png'
+import product3 from './assets/imac.png'
+import product4 from './assets/macmini.png'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'Iphone',
+        price: 999.99,
+        prodImage: product1,
+    },
+
+    {
+        id: 2,
+        productName: 'Macbook',
+        price: 1999.99,
+        prodImage: product2,
+    },
+
+    {
+        id: 3,
+        productName: 'Imac',
+        price: 2599.99,
+        prodImage: product3,
+    },
+
+    {
+        id: 4,
+        productName: 'Macmini',
+        price: 699.99,
+        prodImage: product4,
+    }
+
+];

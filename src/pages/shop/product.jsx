bb859f7186385be5fe641a72ce064b01aead395c
henokbekele <henokbekele1997@gmail.com>
2023-04-30 14:50:17 -0500

    fix the no loading of page

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import { ShopContext} from '../../context/shop-context';
 export const Product = (props) => {
     const {id,productName,price,prodImage} = props.data;
+    const {addToCart} = useContext(ShopContext);
   return (
     <div className='product'>
         <img src = {prodImage} alt='images' />
@@ -13,7 +14,8 @@ export const Product = (props) => {
             ${price}
         </p>
         </div>
-        <button className='addToCartBttn'>Add To Cart</button>
+        <button className='addToCartBttn' onClick={()=>addToCart(id)}>Add To Cart</button>
     </div>
-  );
+  )
+
 };

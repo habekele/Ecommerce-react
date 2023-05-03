00a9753a262a4cc02449018b22332cc600dc463e
henokbekele <henokbekele1997@gmail.com>
2023-05-02 22:01:42 -0500

    implemented the quantity value in the cart page

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useContext} from "react";
 import "./cart.css"
+import { ShopContext } from '../../context/shop-context'
+
+
 export const CartItem = (props) => {
   const { id, productName, price, prodImage } = props.data;
+  const { cartItems } = useContext(ShopContext)
+
   return (
     <div className="cartItem">
       <img src={prodImage}></img>
@@ -9,7 +14,8 @@ export const CartItem = (props) => {
         <p>
           <b>{productName}</b>
         </p>
-        <p>${price}</p>
+        <p className="price">${price}</p>
+        <p>Quantity: {cartItems[id]}</p>
       </div>
     </div>
   );

4666c10686299840de9df76f83611e1ca71a9585
henokbekele <henokbekele1997@gmail.com>
2023-05-11 23:19:18 -0500

    fixed it

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Router>
           <Navbar/>
           <Routes>
-            <Route path="/" element={<Shop/>}/>
+            <Route path="/Ecommerce-react" element={<Shop/>}/>
             <Route path="/cart" element={<Cart/>}/>
           </Routes>
         </Router>

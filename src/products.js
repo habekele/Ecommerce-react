374cfe988cb003697645081d4482c870375230df
henokbekele <henokbekele1997@gmail.com>
2023-06-02 19:08:28 -0500

    changed item prices

--- src/products.js
+++ src/products.js
@@ -9,35 +9,35 @@ export const PRODUCTS = [
     {
         id: 1,
         productName: 'IPhone 13 Pro Max',
-        price: 999.99,
+        price: 999.50,
         prodImage: product1,
     },
 
     {
         id: 2,
         productName: 'Macbook Pro',
-        price: 1999.99,
+        price: 1999.50,
         prodImage: product2,
     },
 
     {
         id: 3,
         productName: 'IMac (2020)',
-        price: 2599.99,
+        price: 2599.50,
         prodImage: product3,
     },
 
     {
         id: 4,
         productName: 'Mac Mini (M1)',
-        price: 699.99,
+        price: 699.50,
         prodImage: product4,
     },
 
     {
         id: 5,
         productName: 'Airpods Max',
-        price: 549.00,
+        price: 549.50,
         prodImage: product5,
     },
 
